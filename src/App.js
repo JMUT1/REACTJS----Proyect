c29362ac861a2afe52ecfbf73c8c5730b9a86114
Chema Torres <chemaaa94@gmail.com>
2022-03-30 07:19:46 -0600

    separar componentes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
+import Todo from "./components/todo";
 
 function App() {
-  return <div>
-    <h1>My Todos</h1>
-    <div className="card">
-      <h2>TITLE</h2>
-      <div className="actions">
-      <button className="btn">Delete</button>
-      </div>
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo />
+      <Todo />
+      <Todo />
     </div>
-  </div>;
+  );
 }
 
 export default App;
