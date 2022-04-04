f7548b71723d49f5b25312930d742e7148ee2110
Chema Torres <chemaaa94@gmail.com>
2022-04-04 09:22:49 -0500

    Card component in meetupItems

--- src/meetups/meetupItems.js
+++ src/meetups/meetupItems.js
@@ -1,19 +1,24 @@
 import classes from "./meetupitems.module.css";
+import Card from "../components/layout/ui/Card";
 
-function MeetupItem(props){
-    return <li className={classes.item}>
+function MeetupItem(props) {
+  return (
+    <li className={classes.item}>
+      <Card>
         <div className={classes.image}>
-            <img src={props.image} alt={props.title}/>
+          <img src={props.image} alt={props.title} />
         </div>
         <div className={classes.content}>
-            <h3>{props.title}</h3>
-            <address>{props.address}</address>
-            <p>{props.description}</p>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
         </div>
         <div className={classes.actions}>
-            <button>To Favorites</button>
+          <button>To Favorites</button>
         </div>
+      </Card>
     </li>
-};
+  );
+}
 
 export default MeetupItem;
