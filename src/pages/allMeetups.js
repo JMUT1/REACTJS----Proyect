b851063aa31012926ef171475401340beb58623d
Chema Torres <chemaaa94@gmail.com>
2022-04-06 22:24:14 -0500

    Aprendiendo que hace useEfect

--- src/pages/allMeetups.js
+++ src/pages/allMeetups.js
@@ -1,32 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import MeetupList from "../components/meetups/meetupList";
 
-
-
-function AllMeetupsPage(){
+function AllMeetupsPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-  fetch("https://react-getting-started-48e70-default-rtdb.firebaseio.com/meetups.json",).then( response => {
-    return response.json();
-  }) .then(data => {
-    setIsLoading(false);
-    setLoadedMeetups (data);
-  });
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-getting-started-48e70-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
 
-  if (isLoading){
+        const meetups = []
+
+        for (const key in data){
+          const meetup = {
+            id: key,
+            ...data[key]
+          };
+
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
     return (
       <section>
         <p>Loading...</p>
       </section>
-    )
+    );
   }
 
-    return <section>
-        <h1>All Meetups</h1>
-        <MeetupList meetups={loadedMeetups}/>
-
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
     </section>
+  );
 }
 
 export default AllMeetupsPage;
