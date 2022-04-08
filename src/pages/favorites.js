44d01a7ebb059ee0a91d8316feae4695bf12a944
Chema Torres <chemaaa94@gmail.com>
2022-04-08 16:26:25 -0500

    favorite page

--- src/pages/favorites.js
+++ src/pages/favorites.js
@@ -1,5 +1,23 @@
+import {useContext} from "react";
+import FavoritesContext from "../store/favorite-context";
+import MeetupList from "../components/meetups/meetupList";
+
 function FavoritesPage(){
-    return <div>Favorite Page</div>
+    const favoriteCtx = useContext (FavoritesContext);
+    let content;
+    if(favoriteCtx.totalFavorites === 0){
+        content = <p>You got no favorites yet</p>
+    } else{
+        content =
+        <MeetupList meetups={favoriteCtx.favorites}/>
+    }
+
+
+
+    return <section>
+        <h1>My Favorites</h1>
+        {content}
+        </section>
 }
 
 export default FavoritesPage;
