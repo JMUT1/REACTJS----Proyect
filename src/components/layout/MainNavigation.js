ab0a09ab91b7222bfc832c4fe5a6dfe311ca74bf
Chema Torres <chemaaa94@gmail.com>
2022-04-08 16:31:03 -0500

    Favorites length in navbar

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,13 +1,19 @@
+import { useContext } from "react";
 import {Link} from "react-router-dom"
 import classes from "./MainNavigation.module.css";
+import FavoritesContext from "../../store/favorite-context";
 
 function MainNavigation(){
+    const favoriteCtx = useContext(FavoritesContext)
+
     return <header className={classes.header}>
         <div className={classes.logo}>REACT MEETUPS</div>
         <nav>
             <ul>
                 <li><Link to={"/"}>All Meetups</Link></li>
-                <li><Link to= {"/favorites"}>Favorites</Link></li>
+                <li><Link to= {"/favorites"}>Favorites
+                <span className={classes.badge}>{favoriteCtx.totalFavorites}</span>
+                </Link></li>
                 <li><Link to= {"/new-meetup"}>New Meetup</Link></li>
             </ul>
         </nav>
